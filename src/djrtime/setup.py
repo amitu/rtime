dc258ea5f9bbe697daf6cbcc0b0f9857c3c07d9b
Vinay Karanam <vinayinvicible@gmail.com>
2016-10-31 19:11:59 +0530

    added setup.py

---
+++ src/djrtime/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='djrtime',
+    version='0.1.0a1',
+    packages=['djrtime', 'djrtime.conf'],
+    url='',
+    license='',
+    author='Vinay Karanam',
+    author_email='',
+    description=''
+)
